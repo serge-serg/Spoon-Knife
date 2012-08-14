3c9eaad4142a5f62c2f1325151ecc52aa2d83915
serge-serg <srgg67@gmail.com>
2012-08-14 15:25:10 +0400

    A new piece of text has added

--- yet.php
+++ yet.php
@@ -6,6 +6,8 @@
 </head>
 
 <body>
-OK, fellows. That's better. Although there is lots of questions yet.
+<p>OK, fellows. That's better. Although there is lots of questions yet.
+</p>
+<p><strong>Amigo, did you want here weren't questions at all, did you?</strong></p>
 </body>
 </html>
