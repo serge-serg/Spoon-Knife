5199304b96d580343151ea3cc0dc57750eff23c9
serge-serg <srgg67@gmail.com>
2012-08-14 15:15:08 +0400

    Yet

---
+++ yet.php
@@ -0,0 +1,11 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Документ без названия</title>
+</head>
+
+<body>
+OK, fellows. That's better. Although there is lots of questions yet.
+</body>
+</html>
